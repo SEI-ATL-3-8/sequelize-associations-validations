df0172d4eee10c0f567e08f1aaac4d6fdc3bf250
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-04-01 18:57:08 -0400

    finish associations

--- models/author.js
+++ models/author.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.author.hasMany(models.book);
+      models.author.belongsToMany(models.subject, {through: 'book'})
+      models.author.belongsTo(models.hometown)
     }
   };
   author.init({
