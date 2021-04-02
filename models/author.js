5062ba8d440e1a19e884bd3abb2138d394d90fd5
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-04-02 00:24:19 -0500

    rz homework

--- models/author.js
+++ models/author.js
@@ -7,18 +7,52 @@ module.exports = (sequelize, DataTypes) => {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
+     * The `mo{dels/index` file will call this method automatically.
      */
     static associate(models) {
       // define association here
+      models.author.belongsTo(models.hometown)
+      models.author.hasMany(models.book)
+      models.author.hasMany(models.subject)
     }
   };
   author.init({
-    name: DataTypes.STRING,
-    age: DataTypes.INTEGER,    
-    sex: DataTypes.STRING,
-    email: DataTypes.STRING,
-    website: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING, allowNull: false,
+      validate:{
+        notEmpty:true,
+      }
+    },
+
+    age:{type: DataTypes.INTEGER, allowNull: false,
+      validate:{
+        isNumeric: true,
+        min: 0,
+      }
+    
+    },    
+    sex: {
+      type: DataTypes.STRING, allowNull: false,
+      validate:{
+        isIn: [['M','S','L']],
+      }
+    
+    },
+
+    email: {
+      type: DataTypes.STRING, allowNull: false,
+      validate: {
+        isEmail:true
+      }
+    
+    },
+    website: {
+      type: DataTypes.STRING, allowNull: false,
+      validate:{
+        isUrl: true,
+      }
+    
+    },
     hometownId: DataTypes.INTEGER,
   }, {
     sequelize,
