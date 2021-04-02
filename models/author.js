3c1c9bc4837eccf85fcea587ac0ee5a35e3cb956
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-02 08:04:30 -0500

    morning

--- models/author.js
+++ models/author.js
@@ -10,14 +10,44 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.author.belongsTo(models.hometown)
+      models.author.hasMany(models.book)
+      models.author.belongsToMany(models.subject, {
+        through: 'book'
+      })
     }
   };
   author.init({
-    name: DataTypes.STRING,
-    age: DataTypes.INTEGER,    
-    sex: DataTypes.STRING,
-    email: DataTypes.STRING,
+    // name: DataTypes.STRING,
+    name: {
+      type:DataTypes.STRING, allowNull:false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+      }
+    },    
+    // age: DataTypes.INTEGER,
+    age: {
+      type:DataTypes.INTEGER, allowNull:false,
+      validate:{
+        isNumeric: true,
+        min: 0
+      }
+    },
+    // sex: DataTypes.STRING,
+    sex: {
+      type:DataTypes.STRING, allowNull:false,
+      validate: {
+        isIm: ['M', 'F']
+      }
+    },
+    // email: DataTypes.STRING,
+    email: {
+      type:DataTypes.INTEGER, allowNull:false,
+      validate:{
+        isEmail: true
+      }
+    },
     website: DataTypes.STRING,
     hometownId: DataTypes.INTEGER,
   }, {
