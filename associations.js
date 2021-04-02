d4be2e3d8ccee6220fa65414e86006dc73b6eb37
Derek Ensign <derekensign@gmail.com>
2021-04-01 22:44:05 -0500

    finished associations

--- associations.js
+++ associations.js
@@ -49,24 +49,75 @@ const models = require('./models')
 
 // get the author of Little Women
 
-const authorLW = async () => {
-    const littleWomen = await models.book.findOne({
-        where: {
-            title: 'Little Women'
-        }
-    })
-    const authorLittleWomen = await littleWomen.getAuthor()
-    console.log(authorLittleWomen)
-}
+// const authorLW = async () => {
+//     const littleWomen = await models.book.findOne({
+//         where: {
+//             title: 'Little Women'
+//         }
+//     })
+//     const authorLittleWomen = await littleWomen.getAuthor()
+//     console.log(authorLittleWomen)
+// }
 
-authorLW()
+// authorLW()
 
 
 // get all books in the Childrens Books subject
 
+// const getChildrens = async () => {
+//     const childrens = await models.subject.findOne({
+//         where: {
+//             name: 'Childrens Books'
+//         }
+//     })
+//     const allChildrens = await childrens.getBooks()
+//     console.log(allChildrens)
+// }
+
+// getChildrens()
+
 // Get the subject of the 2001 Space Oddyssey
+
+// const get2001Subject = async () => {
+//     const spaceOdyssey = await models.book.findOne({
+//         where: {
+//             title: '2001: A Space Odyssey'
+//         }
+//     })
+//     const odysseySubject = await spaceOdyssey.getSubject()
+//     console.log(odysseySubject)
+
+// }
+
+// get2001Subject()
+
 
 // get all subjects that Mark Lutz has written on
 
+// const getLutzSubjects = async () => {
+//     const lutz = await models.author.findOne({
+//         where: {
+//             name: 'Mark Lutz'
+//         }
+//     })
+//     const lutzSubjects = await lutz.getSubjects()
+//     console.log(lutzSubjects)
+// }
+
+// getLutzSubjects()
+
+
+
 // get all authors that have written books in the Childrens Books subject
 
+const getChildrensAuthors = async () => {
+    const childrens = await models.subject.findOne({
+        where: {
+            name: 'Childrens Books'
+        }
+    })
+    const childrensAuthors = await childrens.getAuthors()
+    console.log(childrensAuthors)
+}
+
+getChildrensAuthors()
