33947081d1502a97ee59f77aa47aaecaebfc7475
Elias Ramirez <ramirezelias110@gmail.com>
2021-04-02 01:00:48 -0400

    final commit hw

--- models/hometown.js
+++ models/hometown.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.hometown.hasMany(models.author)
     }
   };
   hometown.init({
