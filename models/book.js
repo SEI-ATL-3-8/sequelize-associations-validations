27dcc376885517dad4a191a9357cb927b0cc225f
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-04-01 19:55:44 -0600

    completed associations.

--- models/book.js
+++ models/book.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.book.belogsTo(models.subject)
+      models.book.belongsTo(models.subject)
       models.book.belongsTo(models.author)
     }
   };
