fca902faab5e0961ac7f72faab170aa5ad2f079a
Nick Link <nlink2021@gmail.com>
2021-04-02 02:28:54 -0500

    Nicks association validation hw

--- models/subject.js
+++ models/subject.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.subject.belongsToMany(models.author, {through: 'book'})
+      // someSubject.getAuthors()
+      // someSubject.addAuthor(someAuthor)
+      models.subject.hasMany(models.book)
     }
   };
   subject.init({
